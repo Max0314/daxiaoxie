2c24334f476e74cb67746a977c9bc7c14e8ff1ce
Max0314 <56707019+Max0314@users.noreply.github.com>
2019-11-01 23:46:22 +0800

    1
    1

---
+++ 首字母大写.js
@@ -0,0 +1,16 @@
+function changeFirstLetter(){
+    var array = str.toLowerCase().split(" ")
+    for(var i =0 ;i < array.length; i++){
+        array[i]=array[i][0].toUpperCase()+array[i].substring(1,array[i].length)
+    }
+    var string = array.join(" ")
+    return string
+}
+
+function ss(){
+var af = new Date().getDate().
+console.log(af)
+
+
+}
+console.log(parseInt('23B24')
